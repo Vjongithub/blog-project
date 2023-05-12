d5dfa1cdf0301f44990c783a32286786328f1345
Vijay Bhagwat <iamvijaybhagwat@gmail.com>
2023-05-12 11:20:10 +0530

    added missing annotations

--- src/main/java/om/pluralsight/blog/models/dto/AuthorDto.java
+++ src/main/java/om/pluralsight/blog/models/dto/AuthorDto.java
@@ -4,11 +4,13 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,6 +19,8 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "author")
 public class AuthorDto {
 
 	@Id
